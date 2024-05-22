c811e7896af04cab86e06544e012bec82814cfe1
Lorzware <l.miething@gmx.de>
2024-05-22 20:32:45 +0200

    modified domain to be different

--- custom_components/apsystemsapi_local/const.py
+++ custom_components/apsystemsapi_local/const.py
@@ -1,5 +1,5 @@
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
-DOMAIN = "apsystemsapi_local"
+DOMAIN = "apsystemsapi_local_lorz"
 UPDATE_INTERVAL = "update_interval"
